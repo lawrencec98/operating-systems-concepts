77f1155747a39d7fa538334dfd388e0e36315181
lawrencec98 <lawrencemcatapang@gmail.com>
2025-07-21 21:52:12 +0100

    make change to comments

--- unix_shell/main.c
+++ unix_shell/main.c
@@ -3,7 +3,7 @@ This project consists of designing a program to serve as a shell interface that 
 and then executes each command in a separate process. This implementation will support input and output
 redirection, as well as pipes as a form of IPC between a pair of commands.
 
-The shell shall be called 'lash' (LA-wrence SH-ell)
+I'll call the shell 'lash' (LA-wrence SH-ell)
 
 build: g++ -g -o out.exe main.c
 */
@@ -75,6 +75,7 @@ int main(void)
 				shouldRun = 0;
 				continue;
 			}
+
 			wait(&pid);
 		}
 
